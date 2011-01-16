5929951feac7b491a159b4512af0feb2cbec826f
Vollch <volk_pk@mail.ru>
2011-01-16 10:56:50 +0300

    0.71

--- bukkit/com/bukkit/Vollch/SkillsBlockListener.java
+++ bukkit/com/bukkit/Vollch/SkillsBlockListener.java
@@ -1,6 +1,9 @@
 package com.bukkit.Vollch;
 
 import org.bukkit.BlockDamageLevel;
+import org.bukkit.ItemStack;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.block.BlockDamagedEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlacedEvent;
@@ -16,43 +19,59 @@ public class SkillsBlockListener extends BlockListener {
         if (event.getDamageLevel() == BlockDamageLevel.BROKEN) {
         	SkillsPlayer sp = SkillsPlayer.get(event.getPlayer());
         	
-    		int skill = SkillsProperties.getDestroySkill(event.getBlock().getTypeID());
-    		if(skill < 1)
-    			return;
-    		int tool = SkillsProperties.getItemLevel(event.getPlayer().getItemInHand().getTypeID(), skill);
-    		int durability = SkillsProperties.getBlockDurability(event.getBlock());
-    		if(SkillsProperties.debugOn){
-    			event.getPlayer().sendMessage("Skill - " + Integer.toString(sp.getLevel(skill)) + " Tool - " + Integer.toString(tool) + " Dura - " + Integer.toString(durability));
+    		int skillDestroy = SkillsProperties.getDestroySkill(event.getBlock().getTypeID());
+    		if(skillDestroy > 0){
+	    		int tool = SkillsProperties.getItemLevel(event.getPlayer().getItemInHand().getTypeID(), skillDestroy);
+	    		int durability = SkillsProperties.getBlockDurability(event.getBlock());
+	    		if(SkillsProperties.debugOn){
+	    			event.getPlayer().sendMessage("Skill - " + Integer.toString(sp.getLevel(skillDestroy)) + " Tool - " + Integer.toString(tool) + " Dura - " + Integer.toString(durability));
+	    		}
+	    		if(durability - (sp.getLevel(skillDestroy) + tool) > SkillsProperties.toBroke) {
+	    			event.getPlayer().sendMessage("This block is too hard for you!");
+	    			event.setCancelled(true);
+	    			return;
+	    		}
+	    		else {
+	    			if(sp.getLevel(skillDestroy) + tool >= durability) {
+	    				SkillsProperties.setBlockDurability(event.getBlock(), 0);
+	    				if(SkillsProperties.levelDependentDestroyGain){
+	    					sp.giveExp(skillDestroy, durability);
+	    				}
+	    				else{
+	    					sp.giveExp(skillDestroy, 1);
+	    				}
+	    			}
+	    			else {
+	    				durability -= 1;
+	    				SkillsProperties.setBlockDurability(event.getBlock(), durability);
+	    				if(SkillsProperties.levelDependentDestroyGain){
+	    					sp.giveExp(skillDestroy, 1);
+	    				}
+	    				event.setCancelled(true);
+	    				return;
+	    			}
+	    		}
     		}
-    		if(durability - (sp.getLevel(skill) + tool) > SkillsProperties.toBroke) {
-    			event.getPlayer().sendMessage("This block is too hard for you!");
-    		}
-    		else {
-    			if(sp.getLevel(skill) + tool >= durability) {
-    				SkillsProperties.setBlockDurability(event.getBlock(), 0);
-    				if(SkillsProperties.levelDependentDestroyGain){
-    					sp.giveExp(skill, durability);
+    		int skillGather = SkillsProperties.getGatherSkill(event.getBlock().getTypeID());
+    		if(skillGather > 0){
+    			sp.giveExp(skillGather, 1);
+    			Integer[] items = SkillsProperties.getDrop(event.getBlock(), sp.getLevel(skillGather));
+    			
+    			if(items.length > 0){
+    				if(SkillsProperties.levelDependentGatherGain){
+    					sp.giveExp(skillGather, items.length);
     				}
-    				else{
-    					sp.giveExp(skill, 1);
+    				World world = event.getPlayer().getWorld();
+    				Location loc = new Location(world, event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ());
+    				for(int i = 0; i < items.length; i++){
+    					world.dropItem(loc, new ItemStack(items[i], 1));
     				}
-    				return;
     			}
-    			else {
-    				durability -= 1;
-    				SkillsProperties.setBlockDurability(event.getBlock(), durability);
-    				if(SkillsProperties.levelDependentDestroyGain){
-    					sp.giveExp(skill, 1);
-    				}
-    				event.setCancelled(true);
-    				return;
-    			}
     		}
-    		event.setCancelled(true);
-    		return;
         }
     }
     
+   
     public void onBlockPlaced(BlockPlacedEvent event) {
 		int skill = SkillsProperties.getCreateSkill(event.getBlockPlaced().getTypeID());
 		if(skill < 1){
--- bukkit/com/bukkit/Vollch/SkillsProperties.java
+++ bukkit/com/bukkit/Vollch/SkillsProperties.java
@@ -4,7 +4,10 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 import org.bukkit.Block;
 
@@ -17,6 +20,8 @@ public class SkillsProperties {
 	private static boolean[] Custom = new boolean[100];
 	private static int[] DestroySkill = new int[100];
 	private static int[] CreateSkill = new int[100];
+	private static int[] GatherSkill = new int[100];
+	private static HashMap<Integer, Hashtable<Integer, Double>> gatherItems = new HashMap<Integer, Hashtable<Integer, Double>>();
 	private static int[][] SkillItem;
 	private static int[] BaseExp;
 	private static int[][] SkillExp;
@@ -28,6 +33,7 @@ public class SkillsProperties {
 	public static boolean debugOn;
 	public static boolean levelDependentCreatyGain;
 	public static boolean levelDependentDestroyGain;
+	public static boolean levelDependentGatherGain;
 	public static int toBroke;
 	public static int saveTimer;
 	
@@ -40,8 +46,9 @@ public class SkillsProperties {
 			debugOn = propertiesFile.getBoolean("debugOn", false);
 			levelDependentCreatyGain = propertiesFile.getBoolean("levelDependentCreatyGain", false);
 			levelDependentDestroyGain = propertiesFile.getBoolean("levelDependentDestroyGain", false);
+			levelDependentGatherGain = propertiesFile.getBoolean("levelDependentGatherGain", false);
 			toBroke = propertiesFile.getInt("toBroke", 5);
-			saveTimer = propertiesFile.getInt("saveTimer", 30000);
+			saveTimer = propertiesFile.getInt("saveTimer", 60000);
 		
 			if(propertiesFile.containsKey("Custom")){
 				temp = propertiesFile.getString("Custom").split(",");
@@ -126,6 +133,18 @@ public class SkillsProperties {
 						CreateSkill[Integer.parseInt(str)] = i;
 					}
 				}
+				if(propertiesFile.containsKey(Skills[i] + "Gather")) {
+					temp = propertiesFile.getString(Skills[i] + "Gather").split(",");
+					for(String str : temp) {
+						temp2 = str.split("-");
+						GatherSkill[Integer.parseInt(temp2[0])] = i;
+						Hashtable<Integer, Double> table = new Hashtable<Integer, Double>();
+						for(int ii = 1; ii < temp2.length; ii+=2){
+							table.put(Integer.valueOf(temp2[ii]), Double.valueOf(temp2[ii+1]));
+						}
+						gatherItems.put(Integer.parseInt(temp2[0]), table);
+					}
+				}
 				if(propertiesFile.containsKey(Skills[i] + "Tools")) {
 					temp = propertiesFile.getString(Skills[i] + "Tools").split(",");
 					for(String str : temp) {
@@ -146,12 +165,14 @@ public class SkillsProperties {
 		try {
 			propertiesFile.load();
 			propertiesFile.setString("debugOn", "false");
-			propertiesFile.setString("levelDependentCreatyGain", "false");
+			propertiesFile.setString("levelDependentCreateGain", "false");
 			propertiesFile.setString("levelDependentDestroyGain", "false");
+			propertiesFile.setString("levelDependentGatherGain", "false");
 			propertiesFile.setString("toBroke", "3");
-			propertiesFile.setString("saveTimer", "30000");
+			propertiesFile.setString("saveTimer", "60000");
+			propertiesFile.setString("Custom", "17,26,27,28,29,30,31,32,33,34,35,35");
 			propertiesFile.setString("Durability", "1-2,2-2,3-2,4-2,5-2,7-15,12-2,13-4,14-7,15-3,16-2,17-2,21-6,22-8,24-2,41-2,42-7,43-3,44-3,45-6,47-3,48-6,49-10,56-13,57-15,73-6,74-6,78-2,80-3,82-3,87-10,88-10,89-10");
-			propertiesFile.setString("SkillNames", "Miner,Mason,Woodcutter,Carpenter,Gemcutter,Metalcrafter,Hauler,Clother");
+			propertiesFile.setString("SkillNames", "Miner,Mason,Woodcutter,Carpenter,Gemcutter,Metalcrafter,Hauler,Clother,Harvester");
 			propertiesFile.setString("BaseExp", "0,200,450,750,1100,1500,1950,2450,3000,3600,4250,4950,5700,6500,7350");
 			propertiesFile.setString("BaseRang", "Novice,Adequate,Competent,Skilled,Proficient,Talented,Adept,Expert,Professional,Accomplished,Great,Master,High Master,Grand Master,Legendary");
 			propertiesFile.setString("MinerDestroy", "1,4,7,24,43,44,45,48,49,67,87,89");
@@ -170,8 +191,9 @@ public class SkillsProperties {
 			propertiesFile.setString("HaulerCreate", "2,3,12,13,60,78,80,82,88");
 			propertiesFile.setString("HaulerTools", "277-3,256-2,273-1,269-1,284-1");
 			propertiesFile.setString("HaulerExp", "0,300,700,1200,1800,2500,3300,4200,5200,6300,7500,8800,10200,11700,13300");
-			propertiesFile.setString("ClotherCreate", "26,27,28,29,30,31,32,33,34,35,36");
 			propertiesFile.setString("ClotherDestroy", "26,27,28,29,30,31,32,33,34,35,36");
+			propertiesFile.setString("ClotherCreate", "26,27,28,29,30,31,32,33,34,35,36");
+			propertiesFile.setString("HarvesterGather", "18-5-0.03-18-0.03-260-0.01-323-0.003,37-37-0.2,38-38-0.2,39-39-0.2,40-40-0.2,59-296-0.3-295-0.1");
 			propertiesFile.save();
 			return true;
 		}
@@ -183,6 +205,10 @@ public class SkillsProperties {
 	
 	public static int getDestroySkill(int block) {
 		return DestroySkill[block];
+	}
+	
+	public static int getGatherSkill(int block) {
+		return GatherSkill[block];
 	}
 	
 	public static int getCreateSkill(int block) {
@@ -277,7 +303,23 @@ public class SkillsProperties {
         return hash;
     }
 	
-	
+	public static Integer[] getDrop(Block block, int level){
+		ArrayList<Integer> items = new ArrayList<Integer>();
+		for(int i = 0; i < level; i++){
+			Hashtable<Integer, Double> table = gatherItems.get(block.getTypeID());
+			Enumeration<Integer> keys = table.keys();
+			while(keys.hasMoreElements()) {
+				int item = keys.nextElement();
+				if(Math.random() < table.get(item)){
+					items.add(item);
+				}
+			}
+		}
+		Integer[] ret = new Integer[items.size()];
+		items.toArray(ret);
+		return ret;
+	}
+    
 	public static int getExpForLevel(int level, int skill) {
 		return SkillExp[skill][level];
 	}	
